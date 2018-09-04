ade394972439eeb5ea45afdb12e73068776b0ea4
ahlqvistk <kristoffer.ahlqvist@gmail.com>
2018-09-04 19:03:52 +0200

    Fix GtkFileChooserDialog size

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,8 +1,14 @@
+import Data.Ratio ((%))
 import XMonad
+import XMonad.Hooks.ManageHelpers (doRectFloat)
 import XMonad.Layout.NoBorders (smartBorders)
+import XMonad.Util.WindowProperties (Property (..), propertyToQuery)
+import qualified XMonad.StackSet as W
 
+main :: IO ()
 main = xmonad $ defaultConfig
      { focusFollowsMouse = False
      , layoutHook = smartBorders $ layoutHook defaultConfig
 		 , modMask = mod4Mask
+     , manageHook = propertyToQuery (Role "GtkFileChooserDialog") --> doRectFloat (W.RationalRect (1 % 4) (1 % 4) (1 % 2) (1 % 2))
      }
