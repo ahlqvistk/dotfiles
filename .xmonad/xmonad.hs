2be6e838e4acf443e616e9b993ecbd3743563ab7
ahlqvistk <kristoffer.ahlqvist@gmail.com>
2019-01-27 09:55:20 +0100

    Config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,22 +1,29 @@
+import Data.Map (fromList, union)
 import Data.Ratio ((%))
 import XMonad
 import XMonad.Hooks.ManageHelpers (doRectFloat)
 import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Util.WindowProperties (Property (..), propertyToQuery)
-import qualified XMonad.StackSet as W
-import qualified Data.Map as M
+import XMonad.StackSet -- RationalRect
 
 main :: IO ()
 main = xmonad $ defaultConfig
-     { focusFollowsMouse = False
-     , keys = \c -> mykeys c `M.union` keys defaultConfig c
+     { borderWidth = 2
+     , focusedBorderColor = "#aaa"
+     , focusFollowsMouse = False
+     , keys = \c -> mykeys c `union` keys defaultConfig c
      , layoutHook = smartBorders $ layoutHook defaultConfig
 		 , modMask = mod4Mask
-     , manageHook = propertyToQuery (Role "GtkFileChooserDialog") --> doRectFloat (W.RationalRect (1 % 4) (1 % 4) (1 % 2) (1 % 2))
+     , manageHook = propertyToQuery (Role "GtkFileChooserDialog") --> doRectFloat (RationalRect (1 % 4) (1 % 4) (1 % 2) (1 % 2))
+     , normalBorderColor = "#ddd"
      }
 		 where
-       mykeys (XConfig {modMask = modm}) = M.fromList $
+       mykeys (XConfig {modMask = modm}) = fromList $
          [ ((modm , xK_a), spawn "/data/Scripts/acpi.sh") 
          , ((modm , xK_c), spawn "/data/Scripts/color-picker.sh") 
          , ((modm , xK_d), spawn "/data/Scripts/date.sh") 
+         , ((modm , xK_F10), spawn "amixer -D pulse set Master 1+ toggle") 
+         , ((modm , xK_F11), spawn "amixer -D pulse set Master 1+ 5%-") 
+         , ((modm , xK_F12), spawn "amixer -D pulse set Master 1+ 5%+") 
+         , ((modm , xK_b), spawn "chromium") 
          ]
